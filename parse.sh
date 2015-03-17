e440b7c6da2fabe7fdf0d1eefde564c01f4cb91f
Artem <root@itsec.pro>
2015-03-17 17:18:48 +0300

    Update parse.sh

--- parse.sh
+++ parse.sh
@@ -1,3 +1,3 @@
 cat * |  grep  "УБИ" | grep : | cut -c 21- | rev | cut -c 6- | rev
 awk '{print $0";"}' file  - добавить ; в конце
-$ cat 046 | grep "нарушитель с" | cut -c 37- | rev | cut -c 38- | rev
+paste.exe -d "" threats.txt ta.txt > threats_ta.txt   #объединить угрозы и агентов
