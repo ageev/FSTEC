99417347d380522e646566de0049eb29789100ee
Artem <root@itsec.pro>
2015-03-17 17:07:01 +0300

    Update parse.sh

--- parse.sh
+++ parse.sh
@@ -1,1 +1,3 @@
-cat 111 |  grep  "УБИ" | grep : | cut -c 21- | rev | cut -c 6- | rev
+cat * |  grep  "УБИ" | grep : | cut -c 21- | rev | cut -c 6- | rev
+awk '{print $0";"}' file  - добавить ; в конце
+$ cat 046 | grep "нарушитель с" | cut -c 37- | rev | cut -c 38- | rev
